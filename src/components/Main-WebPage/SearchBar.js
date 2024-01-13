c914561c3153980ce36fddf005c7aa027ea2aaca
Abd Alwahed Haj Omar <93938245+Abdoul111@users.noreply.github.com>
2024-01-13 01:39:16 -0500

    Improving design

--- src/components/Main-WebPage/SearchBar.js
+++ src/components/Main-WebPage/SearchBar.js
@@ -1,14 +1,27 @@
 import React from "react";
 import "./SearchBar.css";
 
-const SearchBar = () => {
+const SearchBar = (getVideoTranscript) => {
+
+
+  //let [text, setText] = setText('');
+  function updateInputValue(evt) {
+    const val = evt.target.value;
+    //setText(val);
+    getVideoTranscript(val);
+  }
+
+
   return (
-    <div className="p-3">
+    <div className="pt-5 pb-4">
       <input
-        class="form-control mr-sm-2 bar-background-grey text-white mt-5 custom-placeholder p-2"
-        type="search"
+        className="form-control mr-sm-2 bar-background-grey text-white custom-placeholder"
+        type="text"
         placeholder="Enter Youtube URL"
         aria-label="Search"
+        style={{border: '1px solid #6F6F6F'}}
+        //value={text}
+        //onChange={evt => updateInputValue(evt)}
       ></input>
     </div>
   );
--- src/components/Main-WebPage/SubmitButton.js
+++ src/components/Main-WebPage/SubmitButton.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-const SubmitButton = () => {
+
+const SubmitButton = (getVideoTranscript) => {
   // must create a function that takes input from searchBox onSubmit
   return (
     <div>
-      <button type="button" class="btn" style={{ backgroundColor: "#DA5B00" }}>
+      <button
+        type="button"
+        className="btn text-white"
+        style={{ backgroundColor: "#DA5B00", fontWeight: '600'}}
+        onClick={() => getVideoTranscript()}
+      >
         Summarize
       </button>
     </div>
