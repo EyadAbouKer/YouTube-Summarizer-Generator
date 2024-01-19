d39a0953fb3a3a7e42c0b9ce881a458c8b3dab7d
EyadAbouKer <126291554+EyadAbouKer@users.noreply.github.com>
2024-01-19 16:54:15 -0500

    python functions

--- api/api.py
+++ api/api.py
@@ -2,32 +2,27 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from youtube_transcript_api import YouTubeTranscriptApi
 
-# app = Flask(__name__)
-# CORS(app)
-
-# @app.route('/api/getURL', methods=['POST'])
-# def getURL():
-#     # try:
-#         URL = request.json
-#         received_URL = URL['URL']
-#         print(received_URL)
-#         return jsonify({"URL": received_URL}), 200
-    
-    
-    
-    
-    
-    # except TypeError:
-    #     return jsonify({"error": "Bad request, JSON data is expected"}), 400
-    # except KeyError:
-    #     return jsonify({"error": "JSON data does not have 'URL' key"}), 400
+#next step is to implement a function that takes the URL and extract the version
+#URLs to be handeled: https://www.youtube.com/watch?v=LL2xYFZ7-6s (total 32 char) and https://youtu.be/LL2xYFZ7-6s?si=-wL8SfQH0Az2JF_5
+#shorts: https://www.youtube.com/shorts/NELdJLUGVPA and https://youtube.com/shorts/NELdJLUGVPA?si=B7eW8JJAowPsPyY8
+#live: https://www.youtube.com/live/JSsputiQCPA?si=jVha5whsvCWGzgKL --> NOTE: CHECK IF GETTING TRANSCRIPT FROM LIVE VIDEO WOULD BE POSSIBLE
 
 app = Flask(__name__)
 CORS(app)
 
 
 
-    
+def URLModifier(fullURL):
+    finalURL =''
+    for i in range(len(fullURL)):
+        j = len(fullURL) - i-1
+        if fullURL[j] == '/':
+           finalURL=  fullURL[j + 1:]
+           break
+    if 'watch?v=' in finalURL:
+        return finalURL[8:]
+    else:
+        return finalURL
 
 
 @app.route('/api/getURL', methods=['GET', 'POST'])
@@ -36,13 +31,8 @@ def getURL():
         received_URL = URL['URL']
         # return jsonify({"URL": received_URL}), 200
         received_URL = str(received_URL) #turning the data to string
-        print(type(received_URL))
+        print(received_URL)
         return received_URL
-
-
-
-
-
 
 
 def dictionaryToString(VideoURL):
@@ -58,8 +48,10 @@ def dictionaryToString(VideoURL):
 def predict():
     # return {'test': "what is your name"}
     # return dictionaryToString(getURL() )
-    temp = getURL()
-    return dictionaryToString(temp)
+    temp = getURL() #get the URL from React
+    ytVersion = URLModifier(temp) # modify the URL to give us only the version
+    print(ytVersion) #print result 
+    return dictionaryToString(ytVersion) #return transcript
 
 
 if __name__=="__main__":
