4d77cf98813253243b04209b5a6d03bff0245086
EyadAbouKer <126291554+EyadAbouKer@users.noreply.github.com>
2024-01-20 14:41:18 -0500

    modified:   api/__pycache__/api.cpython-311.pyc 	modified:   api/api.py

--- api/api.py
+++ api/api.py
@@ -10,17 +10,48 @@ CORS(app)
 def getURL():
         URL = request.json
         received_URL = URL['URL']
+        
         # return jsonify({"URL": received_URL}), 200
         received_URL = str(received_URL) #turning the data to string
-        transcriptDictionaries = YouTubeTranscriptApi.get_transcript(received_URL)
+        finalURL =''
+        finalfinalURL=''
+        for i  in range(len(received_URL)):
+            j = len(received_URL) -i-1
+            if received_URL[j]=='/':
+                finalURL = received_URL[j + 1:]
+                break
+        if 'watch?v=' in finalURL:
+            finalURL= finalURL[8:]
+        else:
+            print(finalURL)
+            finalURL = finalURL    
+        transcriptDictionaries = YouTubeTranscriptApi.get_transcript(finalURL)
         concatenated_names = ""
         for d in transcriptDictionaries:
             concatenated_names += d.get('text') + " "
         # concatenated_names = getURL()
         concatenated_names = concatenated_names.strip()
+       
         return concatenated_names
 
 
 
 if __name__=="__main__":
     app.run(debug=True)
+    
+    
+    
+    #    
+        # for i in range(len(fullURL)):
+        #     j = len(fullURL) - i-1
+        #     if fullURL[j] == '/':
+        #         finalURL=  fullURL[j + 1:]
+        #         break
+        #     print(finalURL)
+        # if 'watch?v=' in finalURL:
+        #     return finalURL[8:]
+        #     print(finalURL)
+        # else:
+        #     print(finalURL)
+        #     return finalURL
+        
