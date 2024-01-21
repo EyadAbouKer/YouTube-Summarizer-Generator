a8a868b1c114a4476fe7a3414bc21e2b073da21f
EyadAbouKer <126291554+EyadAbouKer@users.noreply.github.com>
2024-01-20 19:40:42 -0500

    new file:   src/Audio/Alarm01.wav 	modified:   src/components/Main-WebPage/TextOutputFeild.js 	new file:   src/components/Main-WebPage/chatGPT_key.js

--- src/components/Main-WebPage/TextOutputFeild.js
+++ src/components/Main-WebPage/TextOutputFeild.js
@@ -2,6 +2,7 @@ import "./TextOutputFeild.css";
 import parse from "html-react-parser"; // this will help in converting the html content inside 'summary' variable into a React component.
 import copyIcon from "C:\\Users\\abouk\\Desktop\\Hackathon2024- TheCodFatherCrew\\CONDENSACORE\\ai-summarizer-v.0.0\\src\\assets\\ClipBoard-icon.svg";
 import speaker from "C:\\Users\\abouk\\Desktop\\Hackathon2024- TheCodFatherCrew\\CONDENSACORE\\ai-summarizer-v.0.0\\src\\assets\\speaker.svg";
+import audioFile from "C:\\Users\\abouk\\Desktop\\Hackathon2024- TheCodFatherCrew\\CONDENSACORE\\ai-summarizer-v.0.0\\src\\Audio\\Alarm01.wav";
 function TextOutputFeild({ summary, text }) {
   // this function copy summary to clipboard
   const copyToClipboard = () => {
@@ -13,10 +14,16 @@ function TextOutputFeild({ summary, text }) {
       .catch((err) => {
         console.error("Something went wrong", err);
       });
+
+    // playSound function that takes a pointer to mp3 and play it in the browser
+    // const playSound = () => {
+
+    //         };
   };
   return (
     <>
       <div className="outputFeildHeader mt-4 rounded d-flex justify-content-end ">
+        {/* /------------------------------------clipboard button----------------------------------------/ */}
         <button
           onClick={copyToClipboard}
           type="button"
@@ -24,9 +31,19 @@ function TextOutputFeild({ summary, text }) {
         >
           <img src={copyIcon} alt="" className="pd-1" />
         </button>
+        {/* /----------------------------------------------------------------------------/ */}
+
+        {/* /--------------------------------speaker--------------------------------------------/ */}
+        <audio id="audio" src={audioFile} />
         <button type="button" class="btn btn-outline-warning p-1">
-          <img src={speaker} alt="" className="speaker" />
+          <img
+            src={speaker}
+            alt=""
+            className="speaker"
+            onClick={() => document.getElementById("audio").play()}
+          />
         </button>
+        {/* /----------------------------------------------------------------------------/ */}
       </div>
       <div
         className=" form-control mr-sm-2 text-white custom-placeholder p-3 "
---
+++ src/components/Main-WebPage/chatGPT_key.js
@@ -0,0 +1,2 @@
+const key = "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";
+export default key;
